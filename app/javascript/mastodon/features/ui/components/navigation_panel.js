abef3fb8654bef058e03066793ab0555d775e301
James <abdl.link@protonmail.com>
2019-07-01 01:07:47 +0100

    Fixed menu link badge formatting

--- app/javascript/mastodon/features/ui/components/navigation_panel.js
+++ app/javascript/mastodon/features/ui/components/navigation_panel.js
@@ -28,7 +28,7 @@ const NavigationPanel = () => (
     <a className='column-link column-link--transparent' href='/settings/preferences'><Icon className='column-link__icon' id='cog' fixedWidth /><FormattedMessage id='navigation_bar.preferences' defaultMessage='Preferences' /></a>
     <a className='column-link column-link--transparent' href='/relationships'><Icon className='column-link__icon' id='users' fixedWidth /><FormattedMessage id='navigation_bar.follows_and_followers' defaultMessage='Follows and followers' /></a>
     {!!profile_directory && <a className='column-link column-link--transparent' href='/explore'><Icon className='column-link__icon' id='address-book-o' fixedWidth /><FormattedMessage id='navigation_bar.profile_directory' defaultMessage='Profile directory' /></a>}
-    <a className='column-link column-link--transparent' href='/locations'><Icon className='column-link__icon' id='map-marker' fixedWidth />Locations<span class='column-link__badge'>New</span></a>
+    <a className='column-link column-link--transparent' href='/locations'><Icon className='column-link__icon' id='map-marker' fixedWidth />Locations<span class='column-link__badge'>new</span></a>
 
     {showTrends && <div className='flex-spacer' />}
     {showTrends && <TrendsContainer />}
