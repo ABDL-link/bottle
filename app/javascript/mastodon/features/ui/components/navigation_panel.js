3beabe765c5999d61f07baf26f84edcc963a15da
James <abdl.link@protonmail.com>
2019-09-05 19:47:01 +0100

    Fixed profile_directory and locations links, removed new label from locations

--- app/javascript/mastodon/features/ui/components/navigation_panel.js
+++ app/javascript/mastodon/features/ui/components/navigation_panel.js
@@ -19,6 +19,7 @@ const NavigationPanel = () => (
     <NavLink className='column-link column-link--transparent' to='/favourites'><Icon className='column-link__icon' id='star' fixedWidth /><FormattedMessage id='navigation_bar.favourites' defaultMessage='Favourites' /></NavLink>
     <NavLink className='column-link column-link--transparent' to='/lists'><Icon className='column-link__icon' id='list-ul' fixedWidth /><FormattedMessage id='navigation_bar.lists' defaultMessage='Lists' /></NavLink>
     {profile_directory && <NavLink className='column-link column-link--transparent' to='/directory'><Icon className='column-link__icon' id='address-book-o' fixedWidth /><FormattedMessage id='getting_started.directory' defaultMessage='Profile directory' /></NavLink>}
+    <a className='column-link column-link--transparent' href='/locations'><Icon className='column-link__icon' id='map-marker' fixedWidth /><FormattedMessage id='navigation_bar.location7s' defaultMessage='Locations' /></a>
 
     <ListPanel />
 
@@ -26,8 +27,6 @@ const NavigationPanel = () => (
 
     <a className='column-link column-link--transparent' href='/settings/preferences'><Icon className='column-link__icon' id='cog' fixedWidth /><FormattedMessage id='navigation_bar.preferences' defaultMessage='Preferences' /></a>
     <a className='column-link column-link--transparent' href='/relationships'><Icon className='column-link__icon' id='users' fixedWidth /><FormattedMessage id='navigation_bar.follows_and_followers' defaultMessage='Follows and followers' /></a>
-    {!!profile_directory && <a className='column-link column-link--transparent' href='/explore'><Icon className='column-link__icon' id='address-book-o' fixedWidth /><FormattedMessage id='navigation_bar.profile_directory' defaultMessage='Profile directory' /></a>}
-    <a className='column-link column-link--transparent' href='/locations'><Icon className='column-link__icon' id='map-marker' fixedWidth /><FormattedMessage id='navigation_bar.location7s' defaultMessage='Locations' /><span class='column-link__badge'>new</span></a>
 
     <hr />
 
